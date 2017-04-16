6f798e805db23482b570bd3fca0c2147c9e7da48
Gildor <gildorwang@gmail.com>
2017-04-16 12:43:40 -0700

    Update Water Heater node to v2.1.

--- Nodes/WaterHeater/WaterHeater.ino
+++ Nodes/WaterHeater/WaterHeater.ino
@@ -27,14 +27,7 @@
  * http://www.mysensors.org/build/relay
  */ 
 
-// Enable debug prints to serial monitor
-#define MY_DEBUG 
-
-// Enable and select radio type attached
-#define MY_RADIO_NRF24
-//#define MY_RADIO_RFM69
-
-#define MY_RF24_CHANNEL    117
+#define MY_NODE_ID 1
 
 // Enable repeater functionality for this node
 #define MY_REPEATER_FEATURE
@@ -43,13 +36,18 @@
 #include <MySensors.h>
 
 #define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define SENSOR_ID_1 1
 #define NUMBER_OF_RELAYS 1 // Total number of attached relays
 #define RELAY_ON 0  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 1 // GPIO value to write to turn off attached relay
 
+#define HEARTBEAT_INTERVAL 10000
+unsigned long nextHeartbeatMillis = 0;
+
+MyMessage msgState1(SENSOR_ID_1, V_LIGHT);
 
 void before() { 
-  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
+  for (int sensor = SENSOR_ID_1, pin = RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
     // Then set relay pins in output mode
     pinMode(pin, OUTPUT);   
     // Set relay to last known state (using eeprom storage) 
@@ -58,29 +56,38 @@ void before() {
 }
 
 void setup() {
-  
+  nextHeartbeatMillis = millis() + HEARTBEAT_INTERVAL;
 }
 
 void presentation()  
 {   
   // Send the sketch version information to the gateway and Controller
-  sendSketchInfo("Water heater", "1.0");
+  sendSketchInfo("Water heater", "2.1");
 
-  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
+  for (int sensor = SENSOR_ID_1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
     // Register all sensors to gw (they will be created as child devices)
-    present(sensor, S_BINARY);
+    present(sensor, S_LIGHT);
   }
 }
 
 
 void loop() 
 {
-  
+  unsigned long now = millis();
+  if (now >= nextHeartbeatMillis) {
+    nextHeartbeatMillis = now + HEARTBEAT_INTERVAL;
+    #ifdef MY_DEBUG
+    Serial.print("Sending status as heartbeat.");
+    #endif
+    for (int sensor = SENSOR_ID_1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
+      send(msgState1.set(loadState(sensor)));
+    }
+  }
 }
 
 void receive(const MyMessage &message) {
   // We only expect one type of message from controller. But we better check anyway.
-  if (message.type==V_STATUS) {
+  if (message.type==V_LIGHT) {
      // Change relay state
      digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
      // Store state in eeprom
