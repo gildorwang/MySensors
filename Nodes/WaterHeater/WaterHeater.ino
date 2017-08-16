5e11cf7edca796086b4b353784066191d3dfd069
Gildor <gildorwang@gmail.com>
2017-08-15 23:19:11 -0700

    Added a button and an LED for water heater relay.

--- Nodes/WaterHeater/WaterHeater.ino
+++ Nodes/WaterHeater/WaterHeater.ino
@@ -20,6 +20,9 @@
  *
  * REVISION HISTORY
  * Version 1.0 - Henrik Ekblad
+ * Version 2.0 - Ken - Signature
+ * Version 2.1 - Ken - Heartbeat
+ * Version 2.2 - Ken - Button & LED
  * 
  * DESCRIPTION
  * Example sketch showing how to control physical relays. 
@@ -34,69 +37,101 @@
 
 #include <SPI.h>
 #include <MySensors.h>
+#include <Bounce2.h>
 
-#define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define RELAY_PIN_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
 #define SENSOR_ID_1 1
 #define NUMBER_OF_RELAYS 1 // Total number of attached relays
-#define RELAY_ON 0  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 1 // GPIO value to write to turn off attached relay
+#define RELAY_ON LOW  // GPIO value to write to turn on attached relay
+#define RELAY_OFF HIGH // GPIO value to write to turn off attached relay
+#define BUTTON_ON LOW
+#define BUTTON_OFF HIGH
+#define BUTTON_PIN 4
+#define LED_PIN 5
+#define LED_PIN_ON HIGH
+#define LED_PIN_OFF LOW
 
-#define HEARTBEAT_INTERVAL 10000
+#define HEARTBEAT_INTERVAL 30000
 unsigned long nextHeartbeatMillis = 0;
 
 MyMessage msgState1(SENSOR_ID_1, V_LIGHT);
 
-void before() { 
-  for (int sensor = SENSOR_ID_1, pin = RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
-    // Then set relay pins in output mode
-    pinMode(pin, OUTPUT);   
-    // Set relay to last known state (using eeprom storage) 
-    digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
-  }
-}
+int buttonOldVal = -1;
+Bounce button = Bounce();
 
-void setup() {
-  nextHeartbeatMillis = millis() + HEARTBEAT_INTERVAL;
+void before() { 
+    Serial.println("Initializing...");
+    pinMode(RELAY_PIN_1, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
+    // Set relay to last known state (using eeprom storage) 
+    setRelay(loadState(SENSOR_ID_1));
+    
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    button.attach(BUTTON_PIN);
+    button.interval(5);
+
+    nextHeartbeatMillis = millis() + HEARTBEAT_INTERVAL;
 }
 
 void presentation()  
 {   
-  // Send the sketch version information to the gateway and Controller
-  sendSketchInfo("Water heater", "2.1");
-
-  for (int sensor = SENSOR_ID_1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
-    // Register all sensors to gw (they will be created as child devices)
-    present(sensor, S_LIGHT);
-  }
+    // Send the sketch version information to the gateway and Controller
+    sendSketchInfo("Water heater", "2.2");
+    // Register sensor to gw (they will be created as child devices)
+    present(SENSOR_ID_1, S_LIGHT);
 }
-
 
 void loop() 
 {
-  unsigned long now = millis();
-  if (now >= nextHeartbeatMillis) {
-    nextHeartbeatMillis = now + HEARTBEAT_INTERVAL;
-    #ifdef MY_DEBUG
-    Serial.print("Sending status as heartbeat.");
-    #endif
-    for (int sensor = SENSOR_ID_1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS;sensor++, pin++) {
-      send(msgState1.set(loadState(sensor)));
+    if (readButton() == BUTTON_ON) {
+        setState(!loadState(SENSOR_ID_1));
     }
-  }
+    unsigned long now = millis();
+    if (now >= nextHeartbeatMillis) {
+        nextHeartbeatMillis = now + HEARTBEAT_INTERVAL;
+        #ifdef MY_DEBUG
+        Serial.print("Sending status as heartbeat.");
+        #endif
+        sendState();
+    }
 }
 
 void receive(const MyMessage &message) {
-  // We only expect one type of message from controller. But we better check anyway.
-  if (message.type==V_LIGHT) {
-     // Change relay state
-     digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
-     // Store state in eeprom
-     saveState(message.sensor, message.getBool());
-     // Write some debug info
-     Serial.print("Incoming change for sensor:");
-     Serial.print(message.sensor);
-     Serial.print(", New status: ");
-     Serial.println(message.getBool());
-   } 
+    if (message.type == V_LIGHT && message.sensor == SENSOR_ID_1) {
+        setState(message.getBool());
+    }
 }
 
+void setState(bool state) {
+    Serial.print("Setting sensor state to: ");
+    Serial.println(state);
+    // Change relay state
+    setRelay(state);
+    // Store state in eeprom
+    saveState(SENSOR_ID_1, state);
+    sendState();
+}
+
+void setRelay(bool state) {
+    digitalWrite(RELAY_PIN_1, state ? RELAY_ON : RELAY_OFF);
+    digitalWrite(LED_PIN, state ? LED_PIN_ON : LED_PIN_OFF);
+}
+
+void sendState() {
+    bool state = loadState(SENSOR_ID_1);
+    Serial.print("Sending state: ");
+    Serial.println(state);
+    send(msgState1.set(state));
+}
+
+int readButton() {
+    button.update();
+    int buttonVal = button.read();
+    if (buttonVal != buttonOldVal) {
+        Serial.print("Button: ");
+        Serial.println(buttonVal == BUTTON_ON);
+        buttonOldVal = buttonVal;
+        return buttonVal;
+    }
+    return -1;
+}
