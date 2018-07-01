096a9eb763cb083c3c4b05287ff6190023677235
Gildor <gildorwang@gmail.com>
2018-07-01 00:39:14 -0700

    Update Sprinkler_2 to v3.1

--- Nodes/Sprinkler_2/Sprinkler_2.ino
+++ Nodes/Sprinkler_2/Sprinkler_2.ino
@@ -20,10 +20,13 @@
  */
 
 #define MY_NODE_ID 51
+//#define MY_DEBUG 
+#define MY_DEBUG_VERBOSE_RFM69
 
 // Enable repeater functionality for this node
 #define MY_REPEATER_FEATURE
 
+#include <MySensorsCustomConfig.h>
 #include <SPI.h>
 #include <MySensors.h>
 #include <Bounce2.h>
@@ -33,28 +36,30 @@
 #define RELAY_1_PIN  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
 #define RELAY_1_SENSOR_ID 1 // Sensor ID for the first relay
 #define NUMBER_OF_RELAYS 2 // Total number of attached relays
-#define RELAY_ON LOW  // GPIO value to write to turn on attached relay
-#define RELAY_OFF HIGH // GPIO value to write to turn off attached relay
-#define BUTTON_ON LOW
-#define BUTTON_OFF HIGH
+#define RELAY_ON HIGH  // GPIO value to write to turn on attached relay
+#define RELAY_OFF LOW // GPIO value to write to turn off attached relay
 #define GREEN_BUTTON_PIN 8
-#define RED_BUTTON_PIN 7
+#define RED_BUTTON_PIN 9
 
 #define SENSOR_ID_LCD 0
+
+#define HEARTBEAT_INTERVAL 30000
 
 // LCD wiring:
 // - VCC: 5V
 // - GND: GND
 // - SDA: A4
 // - SCL: A5
-LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x3F for a 16 chars and 2 line display
 
-#define NODE_VERSION "2.6"
+#define NODE_VERSION "3.1"
 
 // LCD backlight on duration
 const unsigned long lcdOnDurationMillis = 5000;
 // Scheduled LCD backlight off time
 unsigned long lcdOffMillis = 0;
+// Scheduled heartbeat time
+unsigned long heartbeatMillis = 0;
 // The maximum watering time limit, in case the controller is down or lost connectivity
 const unsigned long maxWaterDuration = 30L * 60L * 1000L;
 // Scheduled off time for each station
@@ -63,8 +68,6 @@ unsigned long waterOffMillis[NUMBER_OF_RELAYS] = {0};
 const unsigned long startWateringDelay = 5 * 1000;
 // Millis to start manual watering
 unsigned long startWateringMillis = 0;
-
-bool isInitialized = false;
 
 // State of the system
 enum State {
@@ -82,8 +85,6 @@ int selectedStationId = 0;
 // Reader for the green and the red buttons
 Bounce greenButton = Bounce();
 Bounce redButton = Bounce();
-int greenOldVal = -1;
-int redOldVal = -1;
 
 // Message to send to controller for station state change
 MyMessage stationStatusMsg[NUMBER_OF_RELAYS] = {
@@ -104,11 +105,9 @@ void before() {
         saveState(indexToSensorId(index), false);
     }
 
-    pinMode(GREEN_BUTTON_PIN, INPUT_PULLUP);
-    greenButton.attach(GREEN_BUTTON_PIN);
+    greenButton.attach(GREEN_BUTTON_PIN, INPUT_PULLUP);
     greenButton.interval(5);
-    pinMode(RED_BUTTON_PIN, INPUT_PULLUP);
-    redButton.attach(RED_BUTTON_PIN);
+    redButton.attach(RED_BUTTON_PIN, INPUT_PULLUP);
     redButton.interval(5);
 
     setState(ready);
@@ -120,16 +119,15 @@ void presentation()
     sendSketchInfo("Sprinkler 2", NODE_VERSION);
 
     present(SENSOR_ID_LCD, S_INFO);
+    wait(50);
 
     for (int index = 1; index <= NUMBER_OF_RELAYS; index++) {
         // Register all sensors to gw (they will be created as child devices)
         present(indexToSensorId(index), S_BINARY);
+        wait(50);
     }
 
-    if (!isInitialized) {
-        isInitialized = true;
-        msg("Ready", "Node ver: " NODE_VERSION);
-    }
+    msg("Ready", "Node ver: " NODE_VERSION);
 }
 
 void loop()
@@ -139,43 +137,52 @@ void loop()
         Serial.println("Turn off backlight");
         lcdOffMillis = 0;
     }
+    if (millis() > heartbeatMillis) {
+        sendHeartbeat();
+        heartbeatMillis += HEARTBEAT_INTERVAL;
+    }
     switch (state) {
         case ready:
-            if (readGreenButton() == BUTTON_ON) {
+            if (isGreenButtonPushed()) {
                 setState(selecting);
                 selectedStationId = 1;
                 msg("Station selected: ", selectedStationId);
                 delayForStartWatering();
                 return;
             }
-            if (readRedButton() == BUTTON_ON) {
+            if (isRedButtonPushed()) {
                 setState(shutdown);
                 msg("System shutdown", "Green to turn on");
                 return;
             }
             break;
         case selecting:
-            if (readGreenButton() == BUTTON_ON) {
+            if (isGreenButtonPushed()) {
                 if (++selectedStationId > NUMBER_OF_RELAYS) {
                     selectedStationId = 1;
                 }
                 msg("Station selected: ", selectedStationId);
                 delayForStartWatering();
+                return;
             }
-            else if (readRedButton() == BUTTON_ON) {
+            if (isRedButtonPushed()) {
                 cancelManualWatering();
                 msg("Manual watering", "cancelled");
                 setState(ready);
                 return;
             }
-            else if (startWateringMillis != 0 && millis() >= startWateringMillis) {
+            if (startWateringMillis != 0 && millis() >= startWateringMillis) {
                 setStation(selectedStationId, HIGH);
                 setState(watering);
                 return;
             }
             break;
         case watering:
-            if (readRedButton() == BUTTON_ON) {
+            if (isGreenButtonPushed()) {
+                lcdlight();
+                return;
+            }
+            if (isRedButtonPushed()) {
                 Serial.println("Manually stopped");
                 stopAllWatering();
                 setState(ready);
@@ -197,7 +204,7 @@ void loop()
             }
             break;
         case shutdown:
-            if (readGreenButton() == BUTTON_ON) {
+            if (isGreenButtonPushed()) {
                 setState(ready);
                 msg("Ready", "");
             }
@@ -205,32 +212,28 @@ void loop()
     }
 }
 
-int readGreenButton() {
+int isGreenButtonPushed() {
     greenButton.update();
-    int greenVal = greenButton.read();
-    if (greenVal != greenOldVal) {
+    int greenVal = greenButton.fell();
+    if (greenVal) {
         Serial.print("Green button:");
         Serial.println(greenVal);
-        greenOldVal = greenVal;
-        return greenVal;
     }
-    return -1;
+    return greenVal;
 }
 
-int readRedButton() {
+int isRedButtonPushed() {
     redButton.update();
-    int redVal = redButton.read();
-    if (redVal != redOldVal) {
+    int redVal = redButton.fell();
+    if (redVal) {
         Serial.print("Red button:");
         Serial.println(redVal);
-        redOldVal = redVal;
-        return redVal;
     }
-    return -1;
+    return redVal;
 }
 
 void receive(const MyMessage &message) {
-    if (message.type==V_STATUS) {
+    if (message.type == V_STATUS) {
         int sensor = message.sensor;
         int value = message.getBool();
         int stationId = sensorIdToIndex(sensor);
@@ -352,24 +355,27 @@ void setState(State newState) {
     state = newState;
 }
 
+void lcdlight() {
+    lcd.backlight();
+    lcdOffMillis = millis() + lcdOnDurationMillis;
+}
+
 void msg(const char line1[], const char line2[]) {
     Serial.println(line1);
     Serial.println(line2);
     lcd.clear();
-    lcd.backlight();
+    lcdlight();
     lcd.print(line1);
     lcd.setCursor(0,1); //newline
     lcd.print(line2);
-    lcdOffMillis = millis() + lcdOnDurationMillis;
 }
 
 void msg(const char line1[], int line2) {
     Serial.println(line1);
     Serial.println(line2);
     lcd.clear();
-    lcd.backlight();
+    lcdlight();
     lcd.print(line1);
     lcd.setCursor(0,1); //newline
     lcd.print(line2);
-    lcdOffMillis = millis() + lcdOnDurationMillis;
 }
