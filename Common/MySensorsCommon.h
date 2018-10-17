f6e689e24867831816598e4fb062d882c5342388
Gildor <gildorwang@gmail.com>
2018-10-17 00:31:50 -0700

    Check-in the common libraries

---
+++ Common/MySensorsCommon.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <MySensorsCustomConfig.h>
+#include <MySensors.h>
+
+#define MAX_SEND_ATTEMPTS 5
+
+class ISensor {
+public:
+    virtual void setup() { }
+    virtual void present() = 0;
+    virtual bool report() = 0;
+};
+
+class MessageSender
+{
+private:
+    volatile bool _acked = false;
+public:
+    MessageSender() { 
+        
+    }
+
+    bool handleAck(const MyMessage &message) {
+        if (message.isAck()) {
+            this->_acked = true;
+            #ifdef MY_DEBUG
+            Serial.println("ACK received.");
+            #endif
+            return true;
+        }
+        return false;
+    }
+
+    void send(MyMessage& message) {
+        this->_acked = false;
+        for (byte i = 0; i < MAX_SEND_ATTEMPTS; i++) {
+            ::send(message, true);
+            ::wait(40);
+            if (this->_acked) {
+                #ifdef MY_DEBUG
+                Serial.println("GW ACK");
+                #endif
+                break;
+            }
+            #ifdef MY_DEBUG
+            Serial.println("GW NACK");
+            #endif
+            ::wait(40 * (1 << i));
+        }
+    }
+
+    ~MessageSender() { }
+};
+
+
