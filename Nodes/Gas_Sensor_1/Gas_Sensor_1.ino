bb1c79bb6cafeed4782b893deff55f72855c925a
Gildor <gildorwang@gmail.com>
2018-10-16 23:42:07 -0700

    Change GasSensor's update time and handle ACK

--- Nodes/Gas_Sensor_1/Gas_Sensor_1.ino
+++ Nodes/Gas_Sensor_1/Gas_Sensor_1.ino
@@ -17,8 +17,9 @@
 #define         MOTION_SENSOR_PIN            (3)  //define which digital input pin to use for motion sensor
 #define         DHT_PIN                      (4)  //define which digital input pin to use for dht pin
 
-const long UpdateInterval = 10000; // Wait time between reads (in milliseconds)
+const long UpdateInterval = 30000; // Wait time between reads (in milliseconds)
 unsigned long _nextUpdateMillis = 0;
+volatile bool _interruptted = false;
 
 MessageSender _messageSender;
 DhtSensor _dhtSensor(DHT_PIN, CHILD_ID_TEMPERATURE, CHILD_ID_HUMIDITY, _messageSender);
@@ -26,17 +27,25 @@ GasSensor _gasSensor(MQ_SENSOR_ANALOG_PIN, CHILD_ID_MQ, _messageSender);
 MotionSensor _motionSensor(MOTION_SENSOR_PIN, CHILD_ID_MOTION, _messageSender);
 ISensor* _sensors[3] = { &_gasSensor, &_dhtSensor, &_motionSensor };
 
+void isr() {
+    _interruptted = true;
+}
+
 void setup()
 {
+    Serial.println("Setting up sensors...");
     for (ISensor* sensor : _sensors) {
         sensor->setup();
     }
+
+    Serial.println("Setting up interrupt...");
+    attachInterrupt(digitalPinToInterrupt(MOTION_SENSOR_PIN), isr, CHANGE);
 }
 
 void presentation()
 {
     // Send the sketch version information to the gateway and Controller
-    sendSketchInfo("Garage Gas Sensor", "3.0");
+    sendSketchInfo("Garage Gas Sensor", "3.1");
     
     for (ISensor* sensor : _sensors) {
         sensor->present();
@@ -47,12 +56,20 @@ void presentation()
 void loop()
 {
     unsigned long now = millis();
+    if (_interruptted) {
+        _interruptted = false;
+        _motionSensor.report();
+    }
     if (now > _nextUpdateMillis) {
         _nextUpdateMillis = now + UpdateInterval;
         
         for (ISensor* sensor : _sensors) {
             sensor->report();
-            delay(40);
+            wait(40);
         }
     }
+}
+
+void receive(const MyMessage &message) {
+  _messageSender.handleAck(message);
 }
