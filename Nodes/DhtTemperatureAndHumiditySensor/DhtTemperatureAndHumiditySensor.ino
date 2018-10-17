9e1edfcc4a614b6ebe771091c072352a624dcdb9
Gildor <gildorwang@gmail.com>
2018-10-16 23:43:17 -0700

    Update DHT1 node to use modules

--- Nodes/DhtTemperatureAndHumiditySensor/DhtTemperatureAndHumiditySensor.ino
+++ Nodes/DhtTemperatureAndHumiditySensor/DhtTemperatureAndHumiditySensor.ino
@@ -36,119 +36,41 @@
 // Enabled repeater feature for this node
 #define MY_REPEATER_FEATURE
 
-#include <MySensorsCustomConfig.h>
-#include <SPI.h>
-#include <MySensors.h>  
-#include <DHT.h>
+#include <MySensorsCommon.h>
+#include <DhtSensor.h>
 
 // Set this to the pin you connected the DHT's data pin to
 #define DHT_DATA_PIN 3
-
-// Set this offset if the sensor has a permanent small offset to the real temperatures
-#define SENSOR_TEMP_OFFSET 0
-
-// Sleep time between sensor updates (in milliseconds)
-// Must be >1000ms for DHT22 and >2000ms for DHT11
-static const uint64_t UPDATE_INTERVAL = 5000;
-
-// Force sending an update of the temperature after n sensor reads, so a controller showing the
-// timestamp of the last update doesn't show something like 3 hours in the unlikely case, that
-// the value didn't change since;
-// i.e. the sensor would force sending an update every UPDATE_INTERVAL*FORCE_UPDATE_N_READS [ms]
-static const uint8_t FORCE_UPDATE_N_READS = 10;
 
 #define CHILD_ID_HUM 0
 #define CHILD_ID_TEMP 1
 #define LED_PIN 4
 
-float lastTemp;
-float lastHum;
-bool metric = false;
-unsigned long nextUpdateMillis;
-unsigned long ledOffMillis;
+unsigned long _nextUpdateMillis = 0;
+MessageSender _messageSender;
+DhtSensor _dhtSensor(DHT_DATA_PIN, CHILD_ID_TEMP, CHILD_ID_HUM, _messageSender);
 
-MyMessage msgHum(CHILD_ID_HUM, V_HUM);
-MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
-DHT dht;
-
-
-void presentation()  
-{ 
-  // Send the sketch version information to the gateway
-  sendSketchInfo("TemperatureAndHumidity_1", "3.0");
-  
-  // Register all sensors to gw (they will be created as child devices)
-  present(CHILD_ID_HUM, S_HUM);
-  present(CHILD_ID_TEMP, S_TEMP);
-  
-  metric = getControllerConfig().isMetric;
-}
-
-
-void setup()
+const long UpdateInterval = 30000; // Wait time between reads (in milliseconds)
+void presentation()
 {
-  pinMode(LED_PIN, OUTPUT);
-  
-  dht.setup(DHT_DATA_PIN); // set data pin of DHT sensor
-  if (UPDATE_INTERVAL <= dht.getMinimumSamplingPeriod()) {
-    Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
-  }
-
-  nextUpdateMillis = millis() + UPDATE_INTERVAL;
+    // Send the sketch version information to the gateway
+    sendSketchInfo("TemperatureAndHumidity_1", "3.1");
+    _dhtSensor.present();
 }
 
+void setup() {
+    _dhtSensor.setup();
+}
 
-void loop()      
-{  
-  unsigned long currentMillis = millis();
-  if (currentMillis >= ledOffMillis) {
-    digitalWrite(LED_PIN, LOW);
-  }
-  if (currentMillis >= nextUpdateMillis) {
-    nextUpdateMillis = currentMillis + UPDATE_INTERVAL;
-    
-    // Force reading sensor, so it works also after sleep()
-    dht.readSensor(true);
-    
-    bool isWorking = true;
-    
-    // Get temperature from DHT library
-    float temperature = dht.getTemperature();
-    if (isnan(temperature)) {
-      isWorking = false;
-      Serial.println("Failed reading temperature from DHT!");
-    } else {
-      lastTemp = temperature;
-      if (!metric) {
-        temperature = dht.toFahrenheit(temperature);
-      }
-      temperature += SENSOR_TEMP_OFFSET;
-      send(msgTemp.set(temperature, 1));
-  
-      #ifdef MY_DEBUG
-      Serial.print("T: ");
-      Serial.println(temperature);
-      #endif
+void loop() {  
+    unsigned long currentMillis = millis();
+    if (currentMillis >= _nextUpdateMillis) {
+        _nextUpdateMillis = currentMillis + UpdateInterval;
+        
+        if (_dhtSensor.report()) {
+            digitalWrite(LED_PIN, HIGH);
+        } else {
+            digitalWrite(LED_PIN, LOW);
+        }
     }
-  
-    // Get humidity from DHT library
-    float humidity = dht.getHumidity();
-    if (isnan(humidity)) {
-      isWorking = false;
-      Serial.println("Failed reading humidity from DHT");
-    } else {
-      lastHum = humidity;
-      send(msgHum.set(humidity, 1));
-      
-      #ifdef MY_DEBUG
-      Serial.print("H: ");
-      Serial.println(humidity);
-      #endif
-    }
-
-    if (isWorking) {
-      digitalWrite(LED_PIN, HIGH);
-      ledOffMillis = currentMillis + 50;
-    }
-  }
 }
